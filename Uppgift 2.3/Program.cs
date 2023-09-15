cef2799fb7506f82f6213c485946ad588ae853cf
molo06002 <molo06002@E-PW0234KQ>
2023-09-15 10:34:58 +0200

    Add project files.

---
+++ Uppgift 2.3/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Uppgift_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 300;
+            int extra = 500;
+            string test = "kostnaden för antal kilometer: ";
+            Console.WriteLine("Hur Många dagar ska du köra?");
+           string x = Console.ReadLine();
+            int y = Convert.ToInt32(x);
+            int m = 500 * y + 300 - 500;
+            Console.WriteLine("Hur många kilometer ska du köra?");
+            string k = Console.ReadLine();
+            int t = Convert.ToInt32(k);
+            int R = t * 1;
+            string pris = "Kostnaden blir: ";
+            Console.WriteLine(pris + m + test + R);
+        }
+    }
+}
